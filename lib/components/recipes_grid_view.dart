f44f3873605c10791b4da01d01a3aa85a5cd8d74
MyatTheingi <myattheingi555@gmail.com>
2022-07-20 13:15:49 +0630

    challenge2: add a new girdview layout

--- lib/components/recipes_grid_view.dart
+++ lib/components/recipes_grid_view.dart
@@ -26,7 +26,8 @@ class RecipesGridView extends StatelessWidget {
         // 4
         itemCount: recipes.length,
         // 5
-        gridDelegate: const  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        gridDelegate: const  SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 500.0),
         itemBuilder: (context, index) {
           // 6
           final simpleRecipe = recipes[index];
